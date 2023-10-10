3fde86dc97603628993a3685a830c60c1a8408c2
Makoto Kobayashi <im@asthriona.com>
2023-10-11 01:35:37 +0200

    feat: :zap: Add port to .env

--- src/app.js
+++ src/app.js
@@ -24,4 +24,4 @@ app.get("/", (req,res)=>{
   res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
-app.listen(3000, () => console.log("App listening on port 3000"));
+app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}`));
